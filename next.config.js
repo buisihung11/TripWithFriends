f87a16ff9a70e43f806924a03c69498603e425a4
HungHCC <bshu@netcompany.com>
2023-08-14 11:07:34 +0700

    add uploadthing.com for image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["localhost", "uploadthing.com"],
+  },
+}
 
 module.exports = nextConfig
